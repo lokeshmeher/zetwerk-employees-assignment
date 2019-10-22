71d53c0f62e5761a60206c3799c099268a98b674
Lokesh Meher <lokeshmeher34@gmail.com>
2019-10-22 16:50:36 +0530

    Load dotenv on development only

---
+++ server/index.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV === 'development') import 'dotenv/config'
+import mongoose from 'mongoose'
+import express from 'express'
+import cors from 'cors'
+import { employeesRouter } from './routes/employees'
+
+
+const mongoUrl = `mongodb+srv://primary:${process.env.MONGO_PASSWORD}@production-sp8hx.mongodb.net/db1?retryWrites=true&w=majority`
+mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+
+var app = express()
+
+app.use(express.json())
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Sorry, something went wrong!')
+})
+
+// Routes
+app.use('/employees', employeesRouter)
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
